bfc20d9df3401e722c28c94d80b6fa62242dc5af
Ailwei <Ailweimaemu@gmail.com>
2024-05-28 12:17:07 +0000

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -2,6 +2,8 @@
 """
 basic Flask app in 0-app.py
 """
+
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -9,8 +11,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    summary
+    """
     return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port="5000", host="0.0.0.0", debug=True)
